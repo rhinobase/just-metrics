ec02cd94c5613a988904d50aeeabefd31cf5738b
mathuraditya724 <mathuraditya724@gmail.com>
2025-12-09 15:27:32 +0100

    init commit

---
+++ .nano-staged.js
@@ -0,0 +1,4 @@
+export default {
+    "./src/**/*.{js,ts}": (api) =>
+        `pnpm dlx @biomejs/biome check --write ${api.filenames.join(" ")}`,
+};
---
+++ src/__tests__/index.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+
+describe("basic tests", () => {
+  test("should be true", () => {
+    expect(true).toBe(true);
+  });
+});
